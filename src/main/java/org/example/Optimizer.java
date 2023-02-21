dfd7d985ab0c58ee5638767e576860ed118a5f0d
BoumouzounaBrahimVall <brahimvall.boumouzouna@etu.fstm.ac.ma>
2023-02-21 18:07:34 +0100

    added the main method

--- src/main/java/org/example/Optimizer.java
+++ src/main/java/org/example/Optimizer.java
@@ -12,5 +12,22 @@ public class Optimizer {
 	 */
 	public Optimizer() {
 	}
+	public static void main(String[] args) {
+
+
+		String query = "SELECT nom,villename FROM Personne,Ville WHERE Personne.idville = Ville.idville AND Ville.region = 'case-settat' AND Personne.a='b' OR Personne.z='z' AND Ville.s='s'";
+		Query req=new Query(query);
+
+		req.remplirArbre();
+		req.DessinerArbre();
+
+
+		//req.parseQuery();
+
+      /* System.out.println(req.getProjectionsList());
+       System.out.println(req.getTables());
+       System.out.println(req.getWhereTokens());*/
+
+	}
 
 }

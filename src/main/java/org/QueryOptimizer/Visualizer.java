beb2e8f6d451687b757df6e2037cb64f7c5a5a90
BoumouzounaBrahimVall <brahimvall.boumouzouna@etu.fstm.ac.ma>
2023-03-31 22:41:45 +0000

    visualized cleaned

--- src/main/java/org/QueryOptimizer/Visualizer.java
+++ src/main/java/org/QueryOptimizer/Visualizer.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.util.*;
-import java.util.List;
+
 
 import javax.swing.*;
 
@@ -38,27 +38,27 @@ public class Visualizer extends JPanel {
     private void drawNode(Node node, int x, int y, Graphics g) {
         String str= node.getData();
         if(str.contains("&&")){
-            String[] strs=str.split("&&");
-            g.drawString(strs[0], x-20, y + 12);
+            String[] parts=str.split("&&");
+            g.drawString(parts[0], x-20, y + 12);
             g.drawString("&&", x-12, y + 24);
             int i=y+24;
-            for (int j=1;j<strs.length-1;j++) {
-                g.drawString(strs[j], x - 20, i += 12);
+            for (int j=1;j<parts.length-1;j++) {
+                g.drawString(parts[j], x - 20, i += 12);
                 g.drawString("&&", x - 12, i += 12);
 
             }
-            g.drawString(strs[strs.length-1], x - 20, i += 12);
+            g.drawString(parts[parts.length-1], x - 20, i += 12);
             y=i;
         }else if(str.contains("OR")&&str.length()>2){
-            String[] strs=str.split("OR");
-            g.drawString(strs[0], x-20, y + 12);
+            String[] parts=str.split("OR");
+            g.drawString(parts[0], x-20, y + 12);
             g.drawString("OR", x-12, y + 24);
             int i=y+24;
-            for (int j=1;j<strs.length-1;j++) {
-                g.drawString(strs[j], x - 20, i += 12);
+            for (int j=1;j<parts.length-1;j++) {
+                g.drawString(parts[j], x - 20, i += 12);
                 g.drawString("OR", x - 12, i += 12);
             }
-            g.drawString(strs[strs.length-1], x - 20, i += 12);
+            g.drawString(parts[parts.length-1], x - 20, i += 12);
             y=i;
         }
 
@@ -127,7 +127,7 @@ public class Visualizer extends JPanel {
             JLabel ruleLabel = new JLabel("Rule applied: "+ op.getTr().reglenames.get(tree) );
             ruleLabel.setForeground(new Color(102, 102, 102));
 
-            JLabel costLabel = new JLabel(" " );//+ minCost + "ms, maxCost: " + maxCost + "ms, count physical tree: " + ls.size());
+            JLabel costLabel = new JLabel(" " );
             costLabel.setFont(new Font("Arial", Font.PLAIN, 14));
             costLabel.setForeground(new Color(102, 102, 102));
 
@@ -162,13 +162,7 @@ public class Visualizer extends JPanel {
             topPanel.add(labelPanel, BorderLayout.CENTER);
 
             JPanel bottomPanel = new JPanel(new BorderLayout());
-            bottomPanel.add(scrollPane, BorderLayout.CENTER);
-
-            JPanel treePanel = new JPanel(new BorderLayout());
-            treePanel.setBorder(BorderFactory.createLineBorder(new Color(102, 102, 102), 1));
-            treePanel.add(topPanel, BorderLayout.NORTH);
-            treePanel.add(subPanel, BorderLayout.CENTER);
-            treePanel.add(bottomPanel, BorderLayout.SOUTH);
+            var treePanel=bottomSection(subPanel, topPanel, bottomPanel, scrollPane);
 
             panel.add(treePanel);
             panel.add(Box.createRigidArea(new Dimension(0, 10)));
@@ -179,120 +173,49 @@ public class Visualizer extends JPanel {
         return scrollPane;
     }
 
-
-    public static void createWindowWithPanels(Set<Node>  trees,Node tr,Estimator es) {
-        // Création de la fenêtre principale
-        JFrame frame = new JFrame("Physical trees   ");
-
-        // Définition de la taille et de la position de la fenêtre
-        frame.setSize(800, 600);
-        frame.setLocationRelativeTo(null);
-
-        // Création du panneau principal qui contiendra tous les autres panneaux
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(0, 1));
-        int i = 1;
-        for (Node tree :trees){
-        // Création des panneaux avec titre et champ de saisie
-        JPanel panel1 = createPanelWithTitleAndInput(es.uniCosts(tree,tr),tree,i);
-        i++;
-
-        // Ajout des panneaux au panneau principal
-        mainPanel.add(panel1);
-    }
-
-        // Ajout du panneau principal à une barre de défilement verticale
-        JScrollPane scrollPane = new JScrollPane(mainPanel);
-        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-        // Ajout de la barre de défilement à la fenêtre principale
-        frame.add(scrollPane, BorderLayout.CENTER);
-
-        // Affichage de la fenêtre principale
-        frame.setVisible(true);
-    }
-
-
-    private static JPanel createPanelWithTitleAndInput(String cost,Node tree,int i) {
-        // Création du panneau avec un titre et un champ de saisie
-        Visualizer subPanel = new Visualizer(tree,Color.DARK_GRAY);
-
-        JLabel treeLabel = new JLabel(" Physical Tree " + (i + 1)+cost);
-        treeLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        treeLabel.setForeground(Color.WHITE);
-        treeLabel.setOpaque(true);
-        treeLabel.setBackground(Color.lightGray);
-        treeLabel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-        JPanel topPanel = new JPanel(new BorderLayout());
-        topPanel.add(treeLabel, BorderLayout.NORTH);
-        topPanel.add(treeLabel, BorderLayout.CENTER);
-
-        JPanel bottomPanel = new JPanel(new BorderLayout());
-        JScrollPane scrollPane = new JScrollPane();
-        bottomPanel.add(scrollPane, BorderLayout.CENTER);
-
-        JPanel treePanel = new JPanel(new BorderLayout());
-        treePanel.setBorder(BorderFactory.createLineBorder(new Color(102, 102, 102), 1));
-        treePanel.add(topPanel, BorderLayout.NORTH);
-        treePanel.add(subPanel, BorderLayout.CENTER);
-        treePanel.add(bottomPanel, BorderLayout.SOUTH);
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        panel.add(treePanel);
-        panel.add(Box.createRigidArea(new Dimension(0, 10)));
-
-
-
-
-        treePanel.add(topPanel, BorderLayout.NORTH);
-        treePanel.add(subPanel, BorderLayout.CENTER);
-        treePanel.add(bottomPanel, BorderLayout.SOUTH);
-        return panel;
-    }
-
     private static JPanel drawnTree(Node node, double cost, String script, Color col,Optimizer op) {
 
-        Visualizer mainsubPanel = new Visualizer(node, col);
+        Visualizer mainSubPanel = new Visualizer(node, col);
         JPanel maintopPanel = new JPanel();
         maintopPanel.setLayout(new BoxLayout(maintopPanel, BoxLayout.Y_AXIS));
 
         String titre = script.equals("Main Tree") ? script : "Optimal Tree";
         // Create labels
-        JLabel maintreeLabel = new JLabel(titre);
-        JTextArea maindescrArea = new JTextArea();
-        maindescrArea.setEditable(false);
-        maindescrArea.setLineWrap(true);
-        maindescrArea.setWrapStyleWord(true);
-        maindescrArea.setForeground(col);
-        maindescrArea.setFont(new Font("", Font.BOLD, 13));
+        JLabel mainTreeLabel = new JLabel(titre);
+        JTextArea mainDescrArea = new JTextArea();
+        mainDescrArea.setEditable(false);
+        mainDescrArea.setLineWrap(true);
+        mainDescrArea.setWrapStyleWord(true);
+        mainDescrArea.setForeground(col);
+        mainDescrArea.setFont(new Font("", Font.BOLD, 13));
         String text = cost > 0 ? "\n- Cost: " + cost + "ms  \n" : "";
         if (!script.equals("Main Tree")) {
             String optimal_script = Node.extractScript(Node.cloneTree(node.getLeft()));
-            optimal_script=script.toUpperCase().split("WHERE")[0] + " WHERE " + optimal_script + " }";
+
+            optimal_script=script.toUpperCase().split("WHERE")[0] + (script.contains("WHERE")?" WHERE " + optimal_script:"") + " }";
             optimal_script= optimal_script.replaceAll("\n","");
             text += "- tip: It would be better if you write the Query this way :\n  { " + optimal_script+"\n";
         }else{
             text="\n- logical trees count : "+((int)-cost)+"\n";
         }
 
-        maindescrArea.setText(text);
+        mainDescrArea.setText(text);
 
-        maintreeLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        maintreeLabel.setForeground(col);
-        maintreeLabel.setOpaque(true);
-        maintreeLabel.setBackground(Color.white);
-        maintreeLabel.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
+        mainTreeLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        mainTreeLabel.setForeground(col);
+        mainTreeLabel.setOpaque(true);
+        mainTreeLabel.setBackground(Color.white);
+        mainTreeLabel.setBorder(BorderFactory.createEmptyBorder(20, 10, 20, 10));
 
-        maintopPanel.add(maintreeLabel);
-        maintopPanel.add(maindescrArea);
+        maintopPanel.add(mainTreeLabel);
+        maintopPanel.add(mainDescrArea);
 
         // Set the width of the JTextArea to match the width of the returned panel
         maintopPanel.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
                 int width = maintopPanel.getWidth();
-                maindescrArea.setPreferredSize(new Dimension(width, maindescrArea.getPreferredSize().height));
+                mainDescrArea.setPreferredSize(new Dimension(width, mainDescrArea.getPreferredSize().height));
             }
         });
 
@@ -315,13 +238,90 @@ public class Visualizer extends JPanel {
         });
         JPanel bottomPanel = new JPanel(new BorderLayout());
         bottomPanel.add(scrollPane, BorderLayout.CENTER);
-        JPanel maintreePanel = new JPanel(new BorderLayout());
-        maintreePanel.setBorder(BorderFactory.createLineBorder(col, 1));
-        maintreePanel.add(maintopPanel, BorderLayout.NORTH);
-        maintreePanel.add(mainsubPanel, BorderLayout.CENTER);
+        JPanel mainTreePanel = new JPanel(new BorderLayout());
+        mainTreePanel.setBorder(BorderFactory.createLineBorder(col, 1));
+        mainTreePanel.add(maintopPanel, BorderLayout.NORTH);
+        mainTreePanel.add(mainSubPanel, BorderLayout.CENTER);
         if (!script.equals("Main Tree")) {
-        maintreePanel.add(bottomPanel, BorderLayout.SOUTH);}
-        return maintreePanel;
+            mainTreePanel.add(bottomPanel, BorderLayout.SOUTH);}
+        return mainTreePanel;
     }
+
+    public static void createWindowWithPanels(Set<Node>  trees,Node tr,Estimator es) {
+        // Creation of the main window
+        JFrame frame = new JFrame("Physical trees   ");
+
+        //Setting window size and position
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+
+        // Creation of the main panel which will contain all the other panels
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(0, 1));
+        int i = 1;
+        for (Node tree :trees){
+        // Creation of panels with title and input field
+        JPanel panel1 = createPanelWithTitleAndInput(es.uniCosts(tree,tr),tree,i);
+        i++;
+
+            // Add panels to the main panel
+        mainPanel.add(panel1);
+    }
+
+        // Add the main panel to a vertical scrollbar
+        JScrollPane scrollPane = new JScrollPane(mainPanel);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        // Add the scroll bar to the main window
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        // Display of the main window
+        frame.setVisible(true);
+    }
+
+
+    private static JPanel createPanelWithTitleAndInput(String cost,Node tree,int i) {
+        // Create the panel with a title and an input field
+        Visualizer subPanel = new Visualizer(tree,Color.DARK_GRAY);
+
+        JLabel treeLabel = new JLabel(" Physical Tree " + (i + 1)+cost);
+        treeLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        treeLabel.setForeground(Color.WHITE);
+        treeLabel.setOpaque(true);
+        treeLabel.setBackground(Color.lightGray);
+        treeLabel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        JPanel topPanel = new JPanel(new BorderLayout());
+        topPanel.add(treeLabel, BorderLayout.NORTH);
+        topPanel.add(treeLabel, BorderLayout.CENTER);
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        JScrollPane scrollPane = new JScrollPane();
+        JPanel treePanel = bottomSection(subPanel, topPanel, bottomPanel, scrollPane);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.add(treePanel);
+        panel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+
+
+
+        treePanel.add(topPanel, BorderLayout.NORTH);
+        treePanel.add(subPanel, BorderLayout.CENTER);
+        treePanel.add(bottomPanel, BorderLayout.SOUTH);
+        return panel;
+    }
+
+    private static JPanel bottomSection(Visualizer subPanel, JPanel topPanel, JPanel bottomPanel, JScrollPane scrollPane) {
+        bottomPanel.add(scrollPane, BorderLayout.CENTER);
+
+        JPanel treePanel = new JPanel(new BorderLayout());
+        treePanel.setBorder(BorderFactory.createLineBorder(new Color(102, 102, 102), 1));
+        treePanel.add(topPanel, BorderLayout.NORTH);
+        treePanel.add(subPanel, BorderLayout.CENTER);
+        treePanel.add(bottomPanel, BorderLayout.SOUTH);
+        return treePanel;
+    }
+
 
 }
